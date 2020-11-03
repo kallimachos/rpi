dd9e802bc11cf28f0fb095375773a8410ccdc034
Brian Moss <kallimachos@gmail.com>
2020-11-03 13:03:38 +1000

    display

---
+++ utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import colorlog
+
+logger = colorlog.getLogger()
+
+
+def logconfig(level="DEBUG"):
+    """Set logging configuration."""
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            "%(log_color)s%(levelname)-8s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s"
+        )
+    )
+    logger.addHandler(handler)
+    logger.setLevel(level)
+    logger.debug(f"Logging set to {level}")
+    return
