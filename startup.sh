97d234e84bbe161500c539e7d0a1ad987d55d60c
Brian Moss <kallimachos@gmail.com>
2021-08-05 16:21:28 +1000

    temp startup

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 python3 /home/pi/rpi/mserver.py </dev/null &>/dev/null &
-python3 /home/pi/rpi/mdisplay.py </dev/null &>/dev/null &
-python3 /home/pi/rpi/mlights.py </dev/null &>/dev/null &
+# python3 /home/pi/rpi/mdisplay.py </dev/null &>/dev/null &
+# python3 /home/pi/rpi/mlights.py </dev/null &>/dev/null &
