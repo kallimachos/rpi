98276647a64cd02c6513af809daebc412d277be7
Brian Moss <kallimachos@gmail.com>
2021-08-05 19:39:07 +1000

    startup

--- startup.sh
+++ startup.sh
@@ -1,4 +1,2 @@
-# python3 /home/pi/rpi/mserver.py </dev/null &>/dev/null &
-# python3 /home/pi/rpi/mdisplay.py </dev/null &>/dev/null &
-python3 /home/pi/rpi/mserver.py &
-python3 /home/pi/rpi/mdisplay.py &
+python3 /home/pi/rpi/mserver.py </dev/null &>/dev/null &
+python3 /home/pi/rpi/mdisplay.py </dev/null &>/dev/null &
