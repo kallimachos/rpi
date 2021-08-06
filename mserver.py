b9d86c7e8925e31084e5d7b550d2af5eebd34e17
Brian Moss <kallimachos@gmail.com>
2021-08-06 22:13:39 +1000

    logging

--- mserver.py
+++ mserver.py
@@ -5,11 +5,11 @@
 import json
 import logging
 from os import getenv
+from time import sleep
 
 from bottle import get, post, request, run
 from dotenv import find_dotenv, load_dotenv
 
-logging.basicConfig(filename="/home/pi/mserver.log", level=logging.DEBUG)
 data = json.dumps({"level": "off", "msg": "", "end": ""})
 
 
@@ -30,6 +30,7 @@ def getlevel():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="/home/pi/mserver.log", level=logging.DEBUG)
     logging.info("load dotenv")
     load_dotenv(find_dotenv())
     RPI_IP = getenv("RPI_IP")
@@ -37,5 +38,5 @@ if __name__ == "__main__":
     logging.info(f"RPI_IP: {RPI_IP}")
     logging.info(f"RPI_PORT: {RPI_PORT}")
     logging.info("run server")
+    sleep(5)
     run(host=RPI_IP, port=RPI_PORT, debug=True, reloader=True)
-    logging.info("exit server")
