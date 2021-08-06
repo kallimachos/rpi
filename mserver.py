b1c1ec3a1be09d837f763b4bd58b1b496760b2c7
Brian Moss <kallimachos@gmail.com>
2021-08-06 21:33:32 +1000

    logging

--- mserver.py
+++ mserver.py
@@ -3,15 +3,13 @@
 """Server for RPi meeting program."""
 
 import json
+import logging
 from os import getenv
 
-import colorlog
 from bottle import get, post, request, run
 from dotenv import find_dotenv, load_dotenv
 
-from utils import logconfig
 
-logger = colorlog.getLogger()
 data = json.dumps({"level": "off", "msg": "", "end": ""})
 
 
@@ -20,19 +18,19 @@ def setlevel():
     """Set level, message, and end time."""
     global data
     data = request.json
-    logger.info(f"Level set: {data}.")
+    logging.info(f"Level set: {data}.")
     return
 
 
 @get("/getlevel")
 def getlevel():
     """Get level and message."""
-    logger.info("Returning level, message, and end time.")
+    logging.info("Returning level, message, and end time.")
     return data
 
 
 if __name__ == "__main__":
-    logconfig()
+    logging.basicConfig(filename="/home/pi/rpi/mserver.log", encoding='utf-8', level=logging.DEBUG)
     load_dotenv(find_dotenv())
     RPI_IP = getenv("RPI_IP")
     RPI_PORT = getenv("RPI_PORT")
